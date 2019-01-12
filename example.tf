3cfd1370c4c6531538d7504c9b1ff189bf8c00bb
angautam <44548801+angautam@users.noreply.github.com>
2019-01-12 12:44:11 +0400

    Update example.tf

--- example.tf
+++ example.tf
@@ -1,11 +1,1 @@
-provider "aws" {
-  access_key = "AKIAJSUG6D2B2F34KXIA"
-  secret_key = "eQlbcMDAcY+g7XlCqLtlxoy9rddKk48Lpi3BwVUE"
-  region     = "us-east-2"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-0cd3dfa4e37921605"
-  instance_type = "t2.micro"
-tags { Name = "newscmtest" }
-}
+aaa
